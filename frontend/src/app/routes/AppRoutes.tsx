b355593b9172e1220e679438680280669d5ca5f8
Matheus Martins Viana <matheusmviana@outlook.com.br>
2024-12-21 12:05:45 -0300

    Feat: Adding a table for accounts by person and include localstorage for data persistence

--- frontend/src/app/routes/AppRoutes.tsx
+++ frontend/src/app/routes/AppRoutes.tsx
@@ -1,7 +1,8 @@
 import React, { lazy } from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { Body } from "../shared/components/Body";
-const Home = lazy(() => import("../pages/home/Home")); //lazy permite carregar componentes sob demanda
+import Finances from "../pages/Finances";
+const Home = lazy(() => import("../pages/Home")); //lazy permite carregar componentes sob demanda
 
 // Simulando um atraso na importação do componente para forçar o loading
 // const Home = lazy(
@@ -18,6 +19,7 @@ export const AppRoutes: React.FC = () => {
       <Routes>
         <Route path="/" element={<Body />}>
           <Route path="/" element={<Home />} />
+          <Route path="/financias" element={<Finances />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- frontend/src/app/shared/components/Header.tsx
+++ frontend/src/app/shared/components/Header.tsx
@@ -1,31 +1,49 @@
 import { useState, useEffect } from "react";
 import styles from "./styles/Header.module.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [showTitle, setShowTitle] = useState(true);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setShowTitle((prev) => !prev);
-    }, 5000);
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
 
-    return () => clearInterval(interval);
-  }, []);
+    let interval;
+    if (windowWidth >= 768) {
+      interval = setInterval(() => {
+        setShowTitle((prev) => !prev);
+      }, 5000);
+    } else {
+      setShowTitle(true)
+    }
+
+    return () => {
+      clearInterval(interval);
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [windowWidth]);
 
   return (
     <header className={styles.headerContainer}>
-      {showTitle ? (
-        <h1
-          className={styles.brandTitle}
-          title="Rastreie suas finanças, alcance seus objetivos."
-        >
-          Money Trackr
-        </h1>
-      ) : (
-        <p className={styles.brandSlogan}>
-          Rastreie suas finanças, alcance seus objetivos.
-        </p>
-      )}
+      <Link to="/">
+        {showTitle ? (
+          <h1
+            className={styles.brandTitle}
+            title="Rastreie suas finanças, alcance seus objetivos."
+          >
+            Money Trackr
+          </h1>
+        ) : (
+          <p className={styles.brandSlogan}>
+            Rastreie suas finanças, alcance seus objetivos.
+          </p>
+        )}
+      </Link>
+      <nav className={styles.navbarContainer}>
+        <Link to="/financias">Financias</Link>
+      </nav>
     </header>
   );
 };
