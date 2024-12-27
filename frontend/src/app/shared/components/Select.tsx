081cce442a123bb55b808c160862b484acc20229
Matheus Martins Viana <matheusmviana@outlook.com.br>
2024-12-26 21:11:22 -0300

    Feat; Adding select styles and reusable

--- frontend/src/app/shared/components/Select.tsx
+++ frontend/src/app/shared/components/Select.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import styles from "./styles/Select.module.css";
 import { IoIosArrowDown } from "react-icons/io";
 
@@ -21,43 +21,85 @@ const Select: React.FC<SelectProps> = ({
   className = "",
 }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedLabel, setSelectedLabel] = useState(placeholder);
+  const [selectedValue, setSelectedValue] = useState<string | number | null>(
+    null
+  );
+
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   const toggleDropdown = () => {
     setIsOpen((prevState) => !prevState);
   };
 
   const handleOptionClick = (option: SelectOption) => {
-    setSelectedLabel(option.label);
+    setSelectedValue(option.value);
     onChange(option.value);
     setIsOpen(false);
   };
 
+  const handleDocumentClick = useCallback((event: MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target as Node)
+    ) {
+      setIsOpen(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener("click", handleDocumentClick);
+    } else {
+      document.removeEventListener("click", handleDocumentClick);
+    }
+    return () => {
+      document.removeEventListener("click", handleDocumentClick);
+    };
+  }, [isOpen, handleDocumentClick]);
+
+  const selectedLabel = selectedValue
+    ? options.find((option) => option.value === selectedValue)?.label ||
+      placeholder
+    : placeholder;
+
   return (
-    <div
-      className={`${styles.dropdown} ${className} ${
-        isOpen ? styles.active : ""
-      }`}
-    >
-      <div className={styles.select} onClick={toggleDropdown}>
-        <span>{selectedLabel}</span>
-        <i className={styles.arrow}>
-          <IoIosArrowDown />
-        </i>
+    <div className={styles.dropdownContainer}>
+      <div
+        className={`${styles.dropdown} ${className} ${
+          isOpen ? styles.active : ""
+        }`}
+        ref={dropdownRef}
+      >
+        <div
+          className={styles.select}
+          onClick={toggleDropdown}
+          role="button"
+          tabIndex={0}
+          aria-expanded={isOpen}
+          aria-label="Dropdown selector"
+        >
+          <span>{selectedLabel}</span>
+          <i className={styles.arrow}>
+            <IoIosArrowDown />
+          </i>
+        </div>
+        {isOpen && (
+          <ul className={styles.dropdownMenu} role="listbox">
+            {options.map((option) => (
+              <li
+                key={option.value}
+                onClick={() => handleOptionClick(option)}
+                role="option"
+                aria-selected={selectedValue === option.value}
+                tabIndex={0}
+                id={option.value.toString()}
+              >
+                {option.label}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
-      {isOpen && (
-        <ul className={styles.dropdownMenu}>
-          {options.map((option) => (
-            <li
-              key={option.value}
-              onClick={() => handleOptionClick(option)}
-              id={option.value.toString()}
-            >
-              {option.label}
-            </li>
-          ))}
-        </ul>
-      )}
     </div>
   );
 };
